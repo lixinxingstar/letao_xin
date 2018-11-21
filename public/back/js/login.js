f379be5c6144a1a43c5122b79c428f644a4bdd2a
xinxing <xinxing@qq.com>
2018-11-21 17:17:49 +0800

    登录功能提交校验完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -17,7 +17,11 @@ $(function(){
                         min:2,
                         max:6,
                         message:"用户名长度必须2-6位",
+                    },
+                    callback:{
+                        message:"用户名不存在"
                     }
+
                 }
 
             },
@@ -31,6 +35,9 @@ $(function(){
                         min:6,
                         max:12,
                         message:"密码长度必须是6-12位",
+                    },
+                    callback:{
+                        message:"密码错误",
                     }
                 }
 
@@ -54,10 +61,10 @@ $(function(){
                     location.href = 'index.html';
                 }
                 if(info.error === 1000){
-                    alert(info.message);
+                   $('#form').data("bootstrapValidator").updateStatus('username','INVALID','callback');
                 }
                 if(info.error === 1001){
-                    alert(info.message);
+                   $('#form').data("bootstrapValidator").updateStatus('password','INVALID','callback');
                 }
             }
 
