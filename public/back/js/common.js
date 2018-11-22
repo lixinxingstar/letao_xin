773da09834e6189a767c6f680e536baa6e60bbc0
xinxing <xinxing@qq.com>
2018-11-22 16:16:30 +0800

    公共的功能完成(侧边栏切换)

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,4 +1,8 @@
 $(function(){
+
+ 
+
+
     // 开启进度条
 
 //     NProgress.start();
@@ -21,10 +25,35 @@ $('#category').click(function(){
     
 
 $('.lt_topbar .icon-left').click(function(){
-    console.log(44444);
+    // console.log(44444);
     $('.lt_aside').toggleClass('hidemenu');
     $('.lt_topbar').toggleClass('hidemenu');
     $('.lt_main').toggleClass('hidemenu');
     
+})
+
+
+// 退出
+
+$('.lt_topbar .icon-right').click(function(){
+    //显示退出模态框
+
+    $('#logoutModal').modal('show');
+
+
+})
+
+$('#logoutBtn').click(function(){
+    $.ajax({
+        type:"get",
+        url:"/employee/employeeLogout",
+        dataType:"json",
+        success:function( info ){
+            console.log(info);
+            if(info.success){
+                location.href = "login.html";
+            }
+        }
+    })
 })
 })
---
+++ public/back/js/isLogin.js
@@ -0,0 +1,15 @@
+$.ajax({
+    type:"get",
+    url:"/employee/checkRootLogin",
+    dataType:"json",
+    success:function( info ){
+        console.log(info);
+        if(info.success){
+          console.log("可以");
+        }
+        if(info.error === 400 ){
+            location.href = "login.html";
+          console.log("错误");
+        }
+    }
+})
